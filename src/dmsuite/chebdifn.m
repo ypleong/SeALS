fcb06cf254603ce616662663c4439a6323146e44
ypleong <ypleong@datascopeanalytics.com>
2017-03-27 18:29:17 -0700

    Added a scaled chebychev differentiation scheme

---
+++ src/dmsuite/chebdifn.m
@@ -0,0 +1,70 @@
+function [x, DM] = chebdifn(N, M, xmin, xmax)
+
+% Without (a) and (b)
+
+%  The function [x, DM] =  chebdif(N,M) computes the differentiation 
+%  matrices D1, D2, ..., DM on Chebyshev nodes. 
+% 
+%  Input:
+%  N:        Size of differentiation matrix.        
+%  M:        Number of derivatives required (integer).
+%  xmin:     lower bound of the domain
+%  xmax:     upper bound of the domain
+%  Note:     0 < M <= N-1.
+%
+%  Output:
+%  DM:       DM(1:N,1:N,ell) contains ell-th derivative matrix, ell=1..M.
+%
+%  The code implements two strategies for enhanced 
+%  accuracy suggested by W. Don and S. Solomonoff in 
+%  SIAM J. Sci. Comp. Vol. 6, pp. 1253--1268 (1994).
+%  The two strategies are (a) the use of trigonometric 
+%  identities to avoid the computation of differences 
+%  x(k)-x(j) and (b) the use of the "flipping trick"
+%  which is necessary since sin t can be computed to high
+%  relative precision when t is small whereas sin (pi-t) cannot.
+%  Note added May 2003:  It may, in fact, be slightly better not to
+%  implement the strategies (a) and (b).   Please consult the following
+%  paper for details:   "Spectral Differencing with a Twist", by
+%  R. Baltensperger and M.R. Trummer, to appear in SIAM J. Sci. Comp. 
+
+%  J.A.C. Weideman, S.C. Reddy 1998.  Help notes modified by 
+%  JACW, May 2003.
+
+if nargin < 3
+    xmin = -1;
+    xmax = 1;   
+end
+
+     I = eye(N);                          % Identity matrix.     
+     L = logical(I);                      % Logical identity matrix.
+
+     k = (0:N-1)';                        % Compute theta vector.
+    th = k*pi/(N-1);
+
+     x = cos(th); % Compute Chebyshev points.
+
+     T = repmat(x,1,N);                
+    DX = T-T';           
+ DX(L) = ones(N,1);                       % Put 1's on the main diagonal of DX.
+
+     C = toeplitz((-1).^k);               % C is the matrix with 
+C(1,:) = C(1,:)*2; C(N,:) = C(N,:)*2;     % entries c(k)/c(j)
+C(:,1) = C(:,1)/2; C(:,N) = C(:,N)/2;
+
+     Z = 1./DX;                           % Z contains entries 1/(x(k)-x(j))  
+  Z(L) = zeros(N,1);                      % with zeros on the diagonal.
+
+     D = eye(N);                          % D contains diff. matrices.
+
+DM = zeros(N,N,M);
+for ell = 1:M
+    D = ell*Z.*(C.*repmat(diag(D),1,N) - D); % Off-diagonals
+    D(L) = -sum(D,2);                            % Correct main diagonal of D
+    DM(:,:,ell) = D;                                   % Store current D in DM
+end
+    
+if ~(xmin == -1 && xmax == 1)
+    x = ((xmax-xmin)*x+xmax+xmin)/2;
+    DM = 2*DM/(xmax-xmin);
+end
