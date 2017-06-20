ee9052855e82d8995237df4434319f98c166d977
ypleong <ypleong@datascopeanalytics.com>
2017-06-20 16:33:36 -0700

    2D linear working

--- test/test_finite_horizon_hjb_euler.m
+++ test/test_finite_horizon_hjb_euler.m
@@ -1,5 +1,9 @@
-% Test finite horizon hjb
+% Test finite horizon hjb, forward euler
 % June 20, 2017
+
+% Note:
+% - Spectral does not work
+% - Domain needs to be large enough to void blowing up at the boundary
 
 addpath(genpath('../src'));
 clear all
@@ -18,20 +22,16 @@ start_whole = tic;
 
 %% Initialization
 
-d = 1;
+d = 2;
 x = sym('x',[d,1]); %do not change
-n = 201; 
+n = [151, 201]; 
 
 h = 0.005;%pi/(2*n); % time step size
 tend = 8;
 tt = 0:h:tend;
-% tplot = 0.15;
-% plotgap = round(tplot/h);
-% h = tplot/plotgap;
-% nplots = round(tend/tplot);
 
-bdim = [-10 10];
-bcon = { {'d', 0, 0} };
+bdim = [-20 20;-20 20];
+bcon = { {'d', 0, 0},{'d', 0, 0} };
 bsca = []; %no manual scaling
 region = [];
 regval = 1;
@@ -50,11 +50,10 @@ fprintf(['Starting run ',num2str(run),' with main_run \n'])
 
 %% Define dynamics
 
-lambda = 1;
 % f = 2*[x(1)^5-x(1)^3-x(1)+x(1)*x(2)^4; x(2)^5-x(2)^3-x(2)+x(2)*x(1)^4]; % dynamics
 % linearized dynamics
-AA = -1; 
-BB = 1; 
+AA = -eye(d); 
+BB = ones(d,1); 
 QQ = eye(d);
 PP = care(AA,BB,QQ);
 
@@ -66,6 +65,7 @@ B = BB;
 noise_cov = 1;
 q = x'*QQ*x;
 R = 1;
+lambda = noise_cov*R;
 
 %% Calculate differentiation operators and finite difference matrices
 
@@ -73,22 +73,22 @@ fprintf('Creating differential operators ...\n');
 start_diff = tic;
 
 for i=1:d
-    grid{i} = linspace(bdim(i,1),bdim(i,2),n)';
-    nd(i) = n;
-    dxd(i) = abs(grid{i}(2) - grid{i}(1));
+    gridT{i} = linspace(bdim(i,1),bdim(i,2),n(i))';
+    nd(i) = n(i);
+    dxd(i) = abs(gridT{i}(2) - gridT{i}(1));
     acc(:,i) = [2,2]';
 end
 
-[D,D2,fd1,fd2] = makediffop(grid,nd,dxd,acc,bcon,region);
+[D,D2,fd1,fd2] = makediffop(gridT,nd,dxd,acc,bcon,region);
 
-% [n,grid,~,D,D2,fd1,fd2] = makediffopspectral(bdim,n,bcon,[]);
+% [n,gridT,~,D,D2,fd1,fd2] = makediffopspectral(bdim,n,bcon,[]);
 toc(start_diff)
 end_diff = toc(start_diff);
 
 %% Calculate dynamics
 fprintf('Creating dynamics operator ...\n');
 start_dynamics = tic;
-[fTens,GTens,BTens,noise_covTens,qTens,RTens] = maketensdyn(f,G,B,noise_cov,q,R,x,grid);
+[fTens,GTens,BTens,noise_covTens,qTens,RTens] = maketensdyn(f,G,B,noise_cov,q,R,x,gridT);
 toc(start_dynamics)
 end_dynamics = toc(start_dynamics);
 
@@ -106,9 +106,9 @@ end_PDEop = toc(start_PDEop);
 if isempty(bsca) == 1 || isempty(regsca) == 1
     
     if sca_ver == 1
-        [bscat, ~] = make_bc_sca_var(op,grid,region,bcon);    
+        [bscat, ~] = make_bc_sca_var(op,gridT,region,bcon);    
     elseif sca_ver == 2
-        [bscat, ~] = make_bc_sca(op,bcon,region,regval,als_options,fd1,grid,x,n);
+        [bscat, ~] = make_bc_sca(op,bcon,region,regval,als_options,fd1,gridT,x,n);
     else
         error('wrong specification on boundary scaling');
     end
@@ -125,8 +125,12 @@ end
 %% Create initial conditions
 fprintf('Creating initial conditions ...\n');
 start_PDEinit = tic;
-init = exp(-x'*PP*x/lambda);
-initTens  = fcell2ftens( fsym2fcell(sym(init) ,x), grid);
+if d == 2
+    init = exp(-x(1)'*PP(1,1)*x(1)/lambda)*exp(-x(2)'*PP(2,2)*x(2)/lambda);
+elseif d == 1
+    init = exp(-x(1)'*PP(1,1)*x(1)/lambda);
+end
+initTens  = fcell2ftens( fsym2fcell(sym(init) ,x), gridT);
 toc(start_PDEinit)
 end_PDEinit = toc(start_PDEinit);
 
@@ -166,16 +170,10 @@ F_all{1} = initTens{1};
 tdata = 0;
 data = double(initTens{1})';
 t = 0;
-% data_true = exp(-100*atan(tan(sqrt(6)*t/5+atan(sqrt(6)*tan(1-grid{1})))/sqrt(6)).^2)';
-% iter_time = zeros(1, nplots*plotgap);
 
+iter_time = zeros(length(tt),1);
 for ind = 2:length(tt)
-%     fprintf([num2str(i) ': '])
-%     for iter = 1:plotgap
-        start_iter = tic;
-%         fprintf([num2str(iter) ' '])
-%         ind = ((i-1)*plotgap+iter+1);
-%         t = t + h;
+    start_iter = tic;
 %         if isempty(als_variant) %original    
 %             [F, err, iter, Fcond, enrich, t_step, illcondmat, maxit, maxrank, F_cell, B_cell, b_cell] = ...
 %                 als_sys(op,bc,F_all{ind-1},tol_err,als_options,debugging, 0);
@@ -184,17 +182,12 @@ for ind = 2:length(tt)
 %             [F, err, iter, Fcond, enrich, t_step, illcondmat, maxit, maxrank, F_cell, B_cell, b_cell, restart] = ...
 %                 als_sys_var(op,bc,F_all{ind-1},tol_err,als_options,als_variant,debugging, 0);
 %         end
-        F = SRMultV(op,F_all{ind-1});
-        [F_all{ind}, ~] = als2(F,tol_err_op);
-        
-%         bc = F2;
-%         F_all{ind} = F2; 
-        iter_time(ind-1) = toc(start_iter);
-%     end
-%     fprintf('\n')
-%     tdata = [tdata; t];
-%     data(i+1,:) = double(F_all{ind})';
-%     data_true(i+1,:) = exp(-100*atan(tan(sqrt(6)*t/5+atan(sqrt(6)*tan(1-grid{1})))/sqrt(6)).^2);
+    F = SRMultV(op,F_all{ind-1});
+    [F_all{ind}, ~] = als2(F,tol_err_op);
+    if mod(ind,10) == 0
+        fprintf('Time: %.4fs  Current tensor rank: %d \n', tt(ind), ncomponents(F_all{ind}))
+    end
+    iter_time(ind-1) = toc(start_iter);
 end
 
 time_solve = toc(start_solve);
@@ -209,16 +202,17 @@ diary off
 
 %% Visualize results
 
-% arrange input data
-% F = arrange(F);
-% plotsolve = {F,err,enrich,t_step,Fcond,grid};
-% plotcomp = {op,err_op,enrich_op,t_step_op,cond_op};
-% plotdebug = {F_cell,b_cell,B_cell};
-
 %% Dimension = 1 
 
 if d == 1
 %% True solution
+
+% AA = -1;
+% BB = 1;
+% QQ = 1;
+% PP = 0.4142;
+% noise = 1;
+% lambda = 1;
 
     xx_nn = 47;
     ts_grid = csvread([dirpath,'grid.csv']);
@@ -237,7 +231,7 @@ if d == 1
     plottend = length(tt);
     figure
     hold on
-    surf(grid{1},tt(end:-1:end-plottend+1),px(:,1:plottend)','EdgeColor','none');
+    surf(gridT{1},tt(end:-1:end-plottend+1),px(:,1:plottend)','EdgeColor','none');
     scatter3(ts_grid(:,1),ts_grid(:,2),ts_value,10,'filled');
     zlim([-0.2 1.2])
     % ylim([7.8 8])
@@ -248,4 +242,28 @@ if d == 1
     colorbar
 end
 
+%% Dimension = 2
 
+if d == 2
+%% Plot result
+    
+    figure
+    ht_pdf = pcolor(gridT{1},gridT{2},double(F_all{1})');
+    h = colorbar;
+    %set(h, 'ylim', [0 0.25])
+    caxis([0 1])
+    set(ht_pdf, 'EdgeColor', 'none');
+    xlabel('x')
+    ylabel('y')
+    title(['Time = ',num2str(tt(1))]);
+    grid on
+    axis ([bdim(1,:),bdim(2,:)])
+    grid on
+    for k = 2:10:length(tt)
+         set ( ht_pdf, 'CData', double(F_all{k})' );
+         title(['Time = ',num2str(tt(k))]);
+         drawnow
+         pause(1.0/1000);
+    end
+
+end
