d8580e1907f44f8098e9d5a2e62127c9ea585b98
Daniel Pastor <danpasmor@gmail.com>
2017-09-07 21:12:23 -0700

    Create a ktensor from a gaussian

---
+++ src/ktensorGaussian.m
@@ -0,0 +1,16 @@
+function [ ktens ] = ktensorGaussian( xMean, diagCov, gridT )
+% Generate a ktensor from a diagonal gaussian pdf
+%
+% xMean  nx1
+% diagCov nx1
+% gridT cell(n,1)
+
+    dim = length(xMean);
+    p0vector = cell(dim,1);
+    for i=1:dim
+       p0vector{i} =  normpdf(gridT{i},xMean(i),sqrt(diagCov(i))); 
+    end
+    ktens= ktensor(p0vector);
+
+end
+
